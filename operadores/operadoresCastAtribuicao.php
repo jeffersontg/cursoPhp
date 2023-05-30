b06a49a072d962ae932be7d7de7225b9ab078c24
Jefferson Teixeira <jefferson.teixeira@dataprev.gov.br>
2023-05-30 20:41:42 -0300

    AULA 04

---
+++ operadores/operadoresCastAtribuicao.php
@@ -0,0 +1,36 @@
+<?php
+# Exemplo de cast
+$variavel1 = "5";
+echo 'O tipo da $variavel1 é: ' . gettype($variavel1);
+echo '<br>';
+
+$variavel1 = (int)$variavel1;
+echo 'Agora o tipo da $variavel1 é: ' . gettype($variavel1);
+
+# Exemplo de atribuição
+echo '<br>';
+$variavel1 += 2;
+echo 'Agora o valor da $variavel1 é: ' . $variavel1;
+
+echo '<br>';
+$variavel1 -= 3;
+echo 'Agora o valor da $variavel1 é: ' . $variavel1;
+
+echo '<br>';
+$variavel1 *= 2;
+echo 'Agora o valor da $variavel1 é: ' . $variavel1;
+
+echo '<br>';
+$variavel1 %= 3;
+echo 'Agora o valor da $variavel1 é: ' . $variavel1;
+
+
+echo '<br>';
+$variavel1 = $variavel1 + (int)"3";
+echo 'Agora o valor da $variavel1 é: ' . $variavel1;
+
+echo '<br>';
+$variavel1 = "5" . "3";
+echo 'Agora o valor da $variavel1 é: ' . $variavel1;
+
+?>
