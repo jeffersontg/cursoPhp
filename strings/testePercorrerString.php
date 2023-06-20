026afebf3dd7cea7045b401faf1646c165d29091
Jefferson Teixeira <jefferson.teixeira@dataprev.gov.br>
2023-06-20 19:43:32 -0300

    Aula 09

--- strings/testePercorrerString.php
+++ strings/testePercorrerString.php
@@ -1,9 +1,9 @@
 <?php
 
-$str = "String";
+$str = "Curso Php";
 
 echo "O Comprimento da variável é " . strlen($str) . "<br>";
-
+     
 for ($i = 0; $i < strlen($str); $i++){
 
     echo "Elemento $i: " . $str[$i] . "<br>";
